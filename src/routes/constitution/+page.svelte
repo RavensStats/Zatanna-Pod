8b3a58ea1f2dd8726199ea6eba2c5de9b891273a
RavensStats <68250748+RavensStats@users.noreply.github.com>
2022-10-24 21:39:19 -0400

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -23,112 +23,112 @@
 <div class="constitution">
     <h1 class="noUnderscore">Marvel VS DC Constitution</h1>
     <p>Welcome everyone,
-
-        • Please read all Rules and Regs before paying, I can answer any questions you may have.
-
-        • League type- 16x 14-team, PPR, 0.5 TEP, 6pt pass TD, 3x random to determine draft order per pod.
-
-        • Buy-in is $25 per year, must pay first 2 years to join. If you trade a 2024 or later pick, payment for that year is due immediately for both owners or trade will be reversed. Each new years payment will be due by February 15th. After that date I'll start trying to fill your slot if you've not paid.
-
-        Important : You can buy into as many pods as youd like. However, if you orphan one of your pods teams you orphan all  of your teams across all pods, so keep that in mind when signing up. The exception to this is if you find the replacement owner for the team youre looking to get rid of. 1 team per pod per person. (max 16)
-
-        • Roster setup 1QB, 2 RB, 3 WR, 1 TE,  3 Flex, 1 SF, 14 bench, 4 taxi, 4 IR.
-
-        • Rookie draft order for 2023 season and beyond for will be determined by reverse standings order for playoff teams and reverse max possible points for all others. Linear draft each year.
-
-        • 4 hour slow draft with clock start 9am EST and will shut off at 11pm EST.
-
-        • Waivers will open up after draft. We will be using FAAB $100 per team. FAAB resets once per year after the playoffs.
-
-        • Friendly banter and shit talking is encouraged, but do not cross the line, we all know where that line is. I reserve the right to kick anyone for anything excessive without a refund.
-
-        Trades will not be manually reviewed, but collusion will not be tolerated. If this happens I reserve the right to kick both owners without a refund.
-
-        • Try to atleast check in every other day, check your trades within reasonable time and try to communicate with other managers.
-
-        The pods will be set to allow over the team limit so that members from other pods can scout, if desired. There will be a lobby for recruiting (this league) and a channel made just for chat to all 224 owners. League wide polls, playoff updates, etc will be posted here. Once someone has committed to join and paid both years to leaguesafe they can pick their pod(s) and be sent the link to their pod(s) and the chat channel. As pods fill they will draft. Ill keep the open slot count posted at all times in the lobby. Do not share the pod links or the chat link externally. If youre recruiting, please send the lobby link.
-
-        Theme: Marvel VS DC
-
-        League structure :
-
-        There will be 16 14-team leagues(pods) that function like a normal dynasty league. Regular season weeks 1-14. Pod playoffs weeks 15-17.  Playoff events weeks 11-17
-
-        Playoff event 1: Cross-pod Playoff Brackets
-        1st place from each pod is seeded into 2 playoff brackets,one Marvel and one DC. Weeks 15-17 the 8 player universe brackets play. After Week 17 scores, the Marvel champion and DC champion go head to head. This will be a cumulutaive points for those two teams during the playoffs. (weeks 15-17)
-
-        Playoff event 2: Mega Bracket
-        The top 128 players by record and tiebreaker will be seeded in a new bracket as of week 10 record.
-        Week 11, 128 play, 64 advance
-        Week 12, 64 play, 32 advance
-        Week 13, 32 play, 16 advance
-        Week 14, 16 play, 8 advance
-        Week 15, 8 play, 4 advance
-        Week 16, 4 play, 2 advance
-        Week 17, 2 play, 1 wins
-
-        Playoff event 3: Guillotine-lite All Play
-        All players will compare their cumulative points in weeks 11-17,with the top X advancing.
-        Week 11, 224 play, 100 advance
-        Week 12, 100 play, 50 advance
-        Week 13, 50 play, 40 advance
-        Week 14, 40 play, 30 advance
-        Week 15, 30 play, 20 advance
-        Week 16, 20 play, 10 advance
-        Week 17, 10 play, 1 wins
-
-        Playoff event 4: Redemption League
-        Each winner of the pod consolation playoffs qualifies for the redemption league the following season. It&amp;apos;s a 16 player redraft league with unique roster and scoring. If a player who qualifies for this doesn&amp;apos;t return the following season an owner will be randomly assigned instead. The format may change year to year at the commissioners discretion. In year 1, this bracket will be populated by the 16 players with the last draft pick(slot 14) after randomization in each pod.
-
-
-        • Payout pool is $5,600 (14 teams x 16 pods x $25)
-
-        Pod prizes:
-        - playoff champion: $100
-        - playoff 2nd place: $50
-        - regular season best record: $25
-        - regular season most points for: $10
-        - regular season longest win streak: $5
-        - regular season most points against: $5
-
-        Playoff event prizes:
-
-        Cross pod bracket
-        - 1st $300
-        - 2nd $150
-        - 3rd-4th $25
-        - all teams from the pod of the winner of the marvel VS DC finals bracket: $5
-        - all teams from Marvel or DC pods based on marvel VS DC finals bracket winner Marvel/DC affiliation: $5
-        - all teams from Hero or Villain pods based on marvel VS DC finals bracket winner hero/villain affiliation: $5
-
-        Mega Bracket
-        -1st $200
-        -2nd $100
-        -3rd &amp;amp; 4th $50
-
-        Guillotine-lite All play
-        -1st $165
-
-        Redemption Bracket
-        - 1st $125
-        - 2nd $50
-        - 3rd-4th $25
-
-        Totals:
-
-        (16 * (100 + 50 + 25 + 10 + 5 + 5)) + 300 + 150 + (2 * 25) + (14 * 5) + (112 * 5) + (112 * 5) + 200 + 100 + (2 * 50) + 165 + 125 + 50 + (2 * 25) = 5600
-
-
-        Tiebreakers are:
-        Record
-        Points for
-        Points against
-        Randomized winner
-
-        Because each pod has the same pool of players, its theoretically possible to face an identical team in the playoff events. If this unlikely scenario is to happen, Tiebreakers will decide the winner.
-
-        🚨 2022 Rookie Class Will Be Included In The Start Up Draft 🚨
-
-        I dont anticipate struggling to fill this, but if we dont get all pods filled prior to start of NFL season then Ill tweak the playoff events and adjust the payouts accordingly. 
+</br>
+        • Please read all Rules and Regs before paying, I can answer any questions you may have.</br>
+</br>
+        • League type- 16x 14-team, PPR, 0.5 TEP, 6pt pass TD, 3x random to determine draft order per pod.</br>
+</br>
+        • Buy-in is $25 per year, must pay first 2 years to join. If you trade a 2024 or later pick, payment for that year is due immediately for both owners or trade will be reversed. Each new years payment will be due by February 15th. After that date I'll start trying to fill your slot if you've not paid.</br>
+</br>
+        Important : You can buy into as many pods as youd like. However, if you orphan one of your pods teams you orphan all  of your teams across all pods, so keep that in mind when signing up. The exception to this is if you find the replacement owner for the team youre looking to get rid of. 1 team per pod per person. (max 16)</br>
+</br>
+        • Roster setup 1QB, 2 RB, 3 WR, 1 TE,  3 Flex, 1 SF, 14 bench, 4 taxi, 4 IR.</br>
+</br>
+        • Rookie draft order for 2023 season and beyond for will be determined by reverse standings order for playoff teams and reverse max possible points for all others. Linear draft each year.</br>
+</br>
+        • 4 hour slow draft with clock start 9am EST and will shut off at 11pm EST.</br>
+</br>
+        • Waivers will open up after draft. We will be using FAAB $100 per team. FAAB resets once per year after the playoffs.</br>
+</br>
+        • Friendly banter and shit talking is encouraged, but do not cross the line, we all know where that line is. I reserve the right to kick anyone for anything excessive without a refund.</br>
+</br>
+        Trades will not be manually reviewed, but collusion will not be tolerated. If this happens I reserve the right to kick both owners without a refund.</br>
+</br>
+        • Try to atleast check in every other day, check your trades within reasonable time and try to communicate with other managers.</br>
+</br>
+        The pods will be set to allow over the team limit so that members from other pods can scout, if desired. There will be a lobby for recruiting (this league) and a channel made just for chat to all 224 owners. League wide polls, playoff updates, etc will be posted here. Once someone has committed to join and paid both years to leaguesafe they can pick their pod(s) and be sent the link to their pod(s) and the chat channel. As pods fill they will draft. Ill keep the open slot count posted at all times in the lobby. Do not share the pod links or the chat link externally. If youre recruiting, please send the lobby link.</br>
+</br>
+        Theme: Marvel VS DC</br>
+</br>
+        League structure :</br>
+</br>
+        There will be 16 14-team leagues(pods) that function like a normal dynasty league. Regular season weeks 1-14. Pod playoffs weeks 15-17.  Playoff events weeks 11-17</br>
+</br>
+        Playoff event 1: Cross-pod Playoff Brackets</br>
+        1st place from each pod is seeded into 2 playoff brackets,one Marvel and one DC. Weeks 15-17 the 8 player universe brackets play. After Week 17 scores, the Marvel champion and DC champion go head to head. This will be a cumulutaive points for those two teams during the playoffs. (weeks 15-17)</br>
+</br>
+        Playoff event 2: Mega Bracket</br>
+        The top 128 players by record and tiebreaker will be seeded in a new bracket as of week 10 record.</br>
+        Week 11, 128 play, 64 advance</br>
+        Week 12, 64 play, 32 advance</br>
+        Week 13, 32 play, 16 advance</br>
+        Week 14, 16 play, 8 advance</br>
+        Week 15, 8 play, 4 advance</br>
+        Week 16, 4 play, 2 advance</br>
+        Week 17, 2 play, 1 wins</br>
+</br>
+        Playoff event 3: Guillotine-lite All Play</br>
+        All players will compare their cumulative points in weeks 11-17,with the top X advancing.</br>
+        Week 11, 224 play, 100 advance</br>
+        Week 12, 100 play, 50 advance</br>
+        Week 13, 50 play, 40 advance</br>
+        Week 14, 40 play, 30 advance</br>
+        Week 15, 30 play, 20 advance</br>
+        Week 16, 20 play, 10 advance</br>
+        Week 17, 10 play, 1 wins</br>
+</br>
+        Playoff event 4: Redemption League</br>
+        Each winner of the pod consolation playoffs qualifies for the redemption league the following season. It&amp;apos;s a 16 player redraft league with unique roster and scoring. If a player who qualifies for this doesn&amp;apos;t return the following season an owner will be randomly assigned instead. The format may change year to year at the commissioners discretion. In year 1, this bracket will be populated by the 16 players with the last draft pick(slot 14) after randomization in each pod.</br>
+</br>
+</br>
+        • Payout pool is $5,600 (14 teams x 16 pods x $25)</br>
+</br>
+        Pod prizes:</br>
+        - playoff champion: $100</br>
+        - playoff 2nd place: $50</br>
+        - regular season best record: $25</br>
+        - regular season most points for: $10</br>
+        - regular season longest win streak: $5</br>
+        - regular season most points against: $5</br>
+</br>
+        Playoff event prizes:</br>
+</br>
+        Cross pod bracket</br>
+        - 1st $300</br>
+        - 2nd $150</br>
+        - 3rd-4th $25</br>
+        - all teams from the pod of the winner of the marvel VS DC finals bracket: $5</br>
+        - all teams from Marvel or DC pods based on marvel VS DC finals bracket winner Marvel/DC affiliation: $5</br>
+        - all teams from Hero or Villain pods based on marvel VS DC finals bracket winner hero/villain affiliation: $5</br>
+</br>
+        Mega Bracket</br>
+        -1st $200</br>
+        -2nd $100</br>
+        -3rd & 4th $50</br>
+</br>
+        Guillotine-lite All play</br>
+        -1st $165</br>
+</br>
+        Redemption Bracket</br>
+        - 1st $125</br>
+        - 2nd $50</br>
+        - 3rd-4th $25</br>
+</br>
+        Totals:</br>
+</br>
+        (16 * (100 + 50 + 25 + 10 + 5 + 5)) + 300 + 150 + (2 * 25) + (14 * 5) + (112 * 5) + (112 * 5) + 200 + 100 + (2 * 50) + 165 + 125 + 50 + (2 * 25) = 5600</br>
+</br>
+</br>
+        Tiebreakers are:</br>
+        Record</br>
+        Points for</br>
+        Points against</br>
+        Randomized winner</br>
+</br>
+        Because each pod has the same pool of players, its theoretically possible to face an identical team in the playoff events. If this unlikely scenario is to happen, Tiebreakers will decide the winner.</br>
+</br>
+        🚨 2022 Rookie Class Will Be Included In The Start Up Draft 🚨</br>
+</br>
+        I dont anticipate struggling to fill this, but if we dont get all pods filled prior to start of NFL season then Ill tweak the playoff events and adjust the payouts accordingly. </br>
     </p>
 </div>
