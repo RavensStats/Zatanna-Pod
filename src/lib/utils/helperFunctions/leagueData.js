6ac23af3996c0298d8a91a4229242a854e2ea3df
RavensStats <68250748+RavensStats@users.noreply.github.com>
2022-10-25 19:35:03 -0400

    Update leagueData.js

--- src/lib/utils/helperFunctions/leagueData.js
+++ src/lib/utils/helperFunctions/leagueData.js
@@ -5,7 +5,7 @@ import { leagueID } from '$lib/utils/leagueInfo';
 export const getLeagueData = async (queryLeagueID = leagueID) => {
 	if(get(leagueData)[queryLeagueID]) {
 		return get(leagueData)[queryLeagueID];	}
-    const res = await fetch(`https://api.sleeper.app/v1/league/829902949487607808`, {compress: true}).catch((err) => { console.error(err); });
+    const res = await fetch(`https://api.sleeper.app/v1/league/${queryLeagueID} `, {compress: true}).catch((err) => { console.error(err); });
 	const data = await res.json().catch((err) => { console.error(err); });
 	
 	if (res.ok) {
